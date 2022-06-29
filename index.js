a7c7d68a4f8f4d82d25bbb85b08e3aa634528896
DannnLim <Dannylim-@hotmail.com>
2022-06-29 16:59:23 +1000

    commit tree 05b15884a958c082bef876db73e25425eb2fa98e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    return drivers.filter(Drive => Drive.toLowerCase() === string.toLowerCase())
+}
+
+// console.log(Drive)
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(Drive => Drive.slice(0, string.length) === string);
+}
+
+console.log(fuzzyMatch())
+
+function matchName(driver, string) {
+    return driver.filter(Drive => Drive.name === string);
+}
